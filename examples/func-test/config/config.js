0383bb63def7b0d88a06e2e02fe857f2e96a0444
JIACHENG9 <zinkey@vip.qq.com>
2019-05-24 19:58:59 +0800

    refactor: plugins

--- examples/func-test/config/config.js
+++ examples/func-test/config/config.js
@@ -18,4 +18,5 @@ export default {
       }
     `,
   ],
+  dva: true,
 };
--- packages/smallfish/src/plugins.ts
+++ packages/smallfish/src/plugins.ts
@@ -4,26 +4,32 @@ const plugins = [
   {
     pluginName: '@smallfish-plugin/script',
     configName: 'script',
+    defaultOptions: false,
   },
   {
     pluginName: '@smallfish-plugin/style',
     configName: 'style',
+    defaultOptions: false,
   },
   {
     pluginName: '@smallfish-plugin/antd',
     configName: 'antd',
+    defaultOptions: true,
   },
   {
     pluginName: '@smallfish-plugin/router',
     configName: 'router',
+    defaultOptions: true,
   },
   {
     pluginName: '@smallfish-plugin/request',
     configName: 'request',
+    defaultOptions: true,
   },
   {
     pluginName: '@smallfish-plugin/dva',
     configName: 'dva',
+    defaultOptions: false,
   },
 ];
 
@@ -35,17 +41,32 @@ export default (api: IApi) => {
       singular: true,
     };
   });
-  plugins.forEach(({ pluginName, configName }) => {
+
+  plugins.forEach(({ pluginName, configName, defaultOptions }) => {
     api._registerConfig(() => () => ({
       name: configName,
       onChange(newConfig) {
-        api.changePluginOption(pluginName, newConfig[configName]);
+        const options = newConfig[configName];
+        if (options) {
+          api.changePluginOption(pluginName, options);
+        } else {
+          api.restart();
+        }
       },
     }));
+
+    let opts;
+    if (config[configName] !== false) {
+      opts = config[configName] || defaultOptions
+    }
     api.registerPlugin({
       id: pluginName,
-      apply: require(pluginName).default,
-      opts: config[configName],
-    });
+      apply(...args) {
+        if (opts) {
+          require(pluginName).default(...args);
+        }
+      },
+      opts,
+    }); 
   });
 };
