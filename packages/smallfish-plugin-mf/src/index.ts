525fb03a704fc8ed1cc10e02adbb829a518bc035
xianxiaow <rocksandy@163.com>
2019-06-05 17:42:48 +0800

    feat: add micro mf plugin

---
+++ packages/smallfish-plugin-mf/src/index.ts
@@ -0,0 +1,36 @@
+import { IApi } from 'umi-types';
+
+export default (api: IApi, options: { tagName: string }) => {
+
+  if (!/^\w+-\w+/.test(options.tagName)) {
+    throw new Error("invalid tagName for custom element")
+  }
+  api.modifyEntryRender(``)
+
+  api.addEntryCode(`
+    class SmallfishEl extends HTMLElement {
+      constructor() {
+        super()
+      }
+      connectedCallback() {
+        const rootContainer = window.g_plugins.apply('rootContainer', {
+          initialValue: React.createElement(require('./router').default),
+        });
+        ReactDOM.render(
+          rootContainer,
+          this,
+        );
+      }
+      disconnectedCallback() {
+        ReactDOM.unmountComponentAtNode(this);
+      }
+      attributeChangedCallback() {}
+    }
+    customElements.define('${options.tagName}', SmallfishEl);
+  `)
+
+  api.modifyHTMLWithAST(($) => {
+    $('body').prepend(`<${options.tagName}/>`);
+    $('#root').remove();
+  });
+}
