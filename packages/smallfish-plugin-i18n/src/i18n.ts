e9d0d02ea53dffc2a4297f416f35cf2b5d2ee635
nikogu <644506165@qq.com>
2019-06-17 16:54:45 +0800

    fix: i18n support custom config

--- packages/smallfish-plugin-i18n/src/i18n.ts
+++ packages/smallfish-plugin-i18n/src/i18n.ts
@@ -1,22 +1,6 @@
 import i18next from 'i18next';
-import languageDetector from 'i18next-browser-languagedetector';
+import i18nextLanguageDetector from 'i18next-browser-languagedetector';
 
-import resources from '@/i18n';
-
-Object.keys(resources).map(key => {
-  resources[key] = {
-    translation: resources[key],
-  };
-});
-
-const params = {
-  resources,
-  fallbackLng: {
-    en: ['en-Us'],
-    default: ['en-US'],
-  },
-};
-
-i18next.use(languageDetector).init(params);
+export const languageDetector = i18nextLanguageDetector;
 
 export default i18next;
